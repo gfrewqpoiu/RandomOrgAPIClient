ede91551c53d123c174404a512de126b747487f0
Kevin Striek <Kevin.Striek@gmail.com>
2019-11-18 15:55:41 +0100

    add generateIntegers function

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ from typing import Tuple
 
 baseurl = "https://api.random.org/json-rpc/2/invoke"
 id = 0
+apikey = ""
 
 
 def prepare_json(method: str, params: dict) -> Tuple[dict, int]:
@@ -21,16 +22,25 @@ def _filter(id: int, result: dict) -> dict:
         return result['result']
 
 
-def check_key(key: str) -> dict:
+def getUsage(key: str) -> dict:
     data, id = prepare_json('getUsage', {'apiKey': key})
     resp = httpx.post(baseurl, json=data)
     return _filter(id, resp.json())
 
+def generateIntegers(amount: int, min: int, max: int, with_replacement: bool=True, base: int = 10):
+    request_json, id = prepare_json("generateIntegers",
+                                {'apiKey': apikey, 'n': amount, 'min': min, 'max': max,
+                                 'replacement': with_replacement, 'base': base})
+    resp = httpx.post(baseurl, json=request_json)
+    resp = _filter(id, resp.json())
+    return resp['random']['data']
 
 if __name__ == '__main__':
     if 'RANDOM_ORG_API_KEY' not in os.environ:
         raise EnvironmentError("Plesse specify your Random.org API key in the environment var: RANDOM_ORG_API_KEY")
-    key = os.environ['RANDOM_ORG_API_KEY']
-    result = check_key(key)
+    apikey = os.environ['RANDOM_ORG_API_KEY']
+    result = getUsage(apikey)
     import pprint
     pprint.pprint(result)
+    integers_res = generateIntegers(10, 1, 6)
+    pprint.pprint(integers_res)
