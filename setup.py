e9478c63bc26e57bfed8be59432c47fde338ea28
Kevin Striek <Kevin.Striek@gmail.com>
2019-11-19 12:40:06 +0100

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+long_desc = "A **very** barebones Random.Org API client for the production API, so Version 2."
+
+setup(
+    name="RandomOrgAPIClient",
+    python_requires=">=3.7",
+    version="0.0.1a",
+    url="https://github.com/gfrewqpoiu/RandomOrgAPIClient",
+    license="The Unlicense",
+    description="Random.Org API Client",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    author="Kevin Striek",
+    author_email="Kevin.Striek@gmail.com",
+    py_modules=["client"],
+    install_requires=[
+        "httpx>=0.7.7",
+    ])
